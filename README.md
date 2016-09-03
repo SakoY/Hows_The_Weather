c3c323f577b6f9bbd5d7105114503ce83f2f44b1
SakoY <rsst123@gmail.com>
2016-09-03 18:20:31 -0400

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Hows_The_Weather
+
+The site tells the weather based off the entered city, by using the OpenWeatherMap API, and also changes the background of the page with a picture of the entered city, by using the Bing API.
+
+animate.css was used to animate the fade in of the body and jQuery was used to create a loading page while the image loads from the Bing API.
+
+Contributions, requests or pull requests are welcome & appreciated!
+
+![alt tag](https://raw.githubusercontent.com/SakoY/Hows_The_Weather/master/images/pic1.png)
+![alt tag](https://raw.githubusercontent.com/SakoY/Hows_The_Weather/master/images/pic2.png)
--- weatherscript.php
+++ weatherscript.php
@@ -4,19 +4,23 @@
   $error = '';
   if ($_GET['city']) {
       $city = str_replace(' ', '', $_GET['city']);
+      // OpenWeatherMap API
       $contents = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q='.$city.'&appid=ENTER API KEY HERE');
       $weatherarray = json_decode($contents, true);
       if ($weatherarray['name'] != '' && $weatherarray['weather']['0']['description'] != '') {
+          //Converted to fahrenheit
           $temp = $weatherarray['main']['temp'] * (9 / 5) - 459.67;
           // Used to replace spaces with '+' to use in api url
           $locationname = str_replace(' ', '+', $weatherarray['name']);
+          // forcast to be outputed
           $forcast = 'The weather in '.$weatherarray['name']." is currently '".$weatherarray['weather']['0']['description']."' with a temperature of ".intval($temp).'°F with a wind speed of '.intval($weatherarray['wind']['speed'] * 2.23694).' MPH';
-
           //CURL used to access Bing API
           $curl = curl_init();
           //hides output
           curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($curl, CURLOPT_URL, 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q='.$locationname.'city+view&count=1&offset=0&mkt=en-us&safeSearch=Moderate&size=large');
+          // Searches " (city name) city " and gets a 'large' picture
+          curl_setopt($curl, CURLOPT_URL, 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q='.$locationname.' city+view&count=1&offset=0&mkt=en-us&safeSearch=Moderate&size=large');
+          // Enter API key
           curl_setopt($curl, CURLOPT_HTTPHEADER, array('Host: api.cognitive.microsoft.com', 'Ocp-Apim-Subscription-Key:ENTER API KEY HERE'));
           $resp = curl_exec($curl);
           curl_close($curl);
@@ -27,3 +31,4 @@
           $error = 'Invalid city please try again';
       }
   }
+?>
